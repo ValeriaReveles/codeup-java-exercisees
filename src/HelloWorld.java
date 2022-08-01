712f2d4bb43e9d5e7b3575f30b399ea15d5960fd
Valeria <valeriareveles12@gmail.com>
2022-08-01 16:54:45 -0500

    finished second java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,27 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print(" Hello, ");
-        System.out.print(" World! ");
+        System.out.print(" World! \n");
+        int myFavoriteNumber = 10;
+        System.out.println(myFavoriteNumber);
+        char myString = 'A';
+        System.out.println(myString);
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+        int three = (int) 3;
+        System.out.println(three);
+        int x = 10;
+        int y = 2;
+//        System.out.println(y);
+        x /= y;
+        System.out.print("x is " + x + "\n");
+        System.out.print("y is " + y + "\n");
+        y -= x;
+        System.out.println(y);
     }
+
 }
+
+
+
+
