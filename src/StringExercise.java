a02ed997f6dc4d42e3913fe269c8ab3190e4817d
Valeria <valeriareveles12@gmail.com>
2022-08-03 14:38:39 -0500

    completed java strings exercise 1 and 2

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,2 +1,10 @@
 public class StringExercise {
+    public static void main(String[] args) {
+        String message = "We don't need no education\nWe don't need no thought control\nCheck \"this\" out!, \"s inside of \"s!\nIn windows, the main drive is usually C:\\\nI can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+//        String message2 = "";
+//        System.out.println(message);
+//        String message2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.print(message);
+
+    }
 }
