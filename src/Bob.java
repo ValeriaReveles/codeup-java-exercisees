18ebdb07ed33dbac81b03157323a611b98e78d6b
Valeria <valeriareveles12@gmail.com>
2022-08-03 15:08:05 -0500

    finished bob exercise

---
+++ src/Bob.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Say something to Bob!");
+        String input = scan.nextLine().trim();
+
+        if(input.endsWith("?")){
+            System.out.println("Sure.");
+        } else if (input.endsWith("!")){
+            System.out.println("Whoa, chill out!");
+        } else if (input.equals("")){
+            System.out.println("Fine. Be that way!");
+        }else{
+            System.out.println("Whatever");
+        }
+    }
+}
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,10 +1,7 @@
 public class StringExercise {
     public static void main(String[] args) {
+        //String exercise 1
         String message = "We don't need no education\nWe don't need no thought control\nCheck \"this\" out!, \"s inside of \"s!\nIn windows, the main drive is usually C:\\\nI can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
-//        String message2 = "";
-//        System.out.println(message);
-//        String message2 = "Check \"this\" out!, \"s inside of \"s!";
         System.out.print(message);
-
     }
 }
