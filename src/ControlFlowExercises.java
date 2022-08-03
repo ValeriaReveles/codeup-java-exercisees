f329eebea3deba66447a4742d1b1e7bc40d5b967
Valeria <valeriareveles12@gmail.com>
2022-08-03 13:37:46 -0500

    created string exercise class

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 import java.text.DecimalFormat;
+import java.util.*;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
@@ -29,18 +30,51 @@ public class ControlFlowExercises {
 //            }
 //        }
 
-        //4. Table
-        DecimalFormat df = new DecimalFormat("0");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("What number would you like to go up to?");
-        String input = scanner.next();
-        System.out.print("Here is your table! \n");
-        System.out.print("number squared cubed\n");
-        int num = parseInt(input);
-//        int x=1;
-//        System.out.println("X^1\tX^2\tX^3\tX^4\tX^5\t");
-        for (int i=1;  i <= num; i++) {
-            System.out.println(df.format(Math.pow(i, 1)) + "\t" + "\t" + df.format(Math.pow(i, 2)) + "\t" + "\t" + df.format(Math.pow(i, 3))  + "\t");
+        //3. Table
+//        DecimalFormat df = new DecimalFormat("0");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("What number would you like to go up to?");
+//        String input = scanner.next();
+//        System.out.print("Here is your table! \n");
+//        System.out.print("number squared cubed\n");
+//        int num = parseInt(input);
+//        for (int i=1;  i <= num; i++) {
+//            System.out.println(df.format(Math.pow(i, 1)) + "\t" + "\t" + df.format(Math.pow(i, 2)) + "\t" + "\t" + df.format(Math.pow(i, 3))  + "\t");
+//        }
+
+        //4. Display grade
+        Scanner grades = new Scanner(System.in);
+        int input = 0;
+        System.out.print("Enter grade received: ");
+        input = grades.nextInt();
+        System.out.println("Here is your grade!");
+        if (input > 100 || input < 0) {
+            System.out.println("Please put in a percent from 0 - 100!\n");
         }
+
+        while (input > 100 || input < 0) {
+
+            System.out.print("What is the percentage of the student: ");
+            input = grades.nextInt();
+            if (input > 100 || input < 0) {
+                System.out.println("Please put in a percent from 0 - 100!\n");
+            }
+        }
+        if (input <= 100 && input >= 88) {
+            System.out.print("A");
+        }
+        if (input <= 87 && input >= 80) {
+            System.out.print("B");
+        }
+        if (input <= 79 && input >= 90) {
+            System.out.print("A-");
+        }
+        if (input <= 89 && input >= 50) {
+            System.out.print("F+");
+        }
+        if (input <= 49 && input >= 0) {
+            System.out.print("F");
+        }
+        // so on and so forth down the grading scale
     }
 }
