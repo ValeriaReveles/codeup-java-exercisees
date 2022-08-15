caae1dbed6b18bbdb283958647dc86dea0a1a9d1
Valeria <valeriareveles12@gmail.com>
2022-08-15 12:05:10 -0500

    updated student exercise number 1

--- src/grades/StudentTest.java
+++ src/grades/StudentTest.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 
 public class StudentTest {
     public static void main(String[]args) {
-        Student Valeria = new Student("Valeria");
-        Valeria.addGrade(80);
-        Valeria.addGrade(90);
-        Valeria.addGrade(100);
-        Valeria.addGrade(70);
-        Valeria.addGrade(60);
-        System.out.println(Valeria.getGradeAverage());
-        System.out.println(Valeria.getGrades());
+        Student valeria = new Student("Valeria");
+        valeria.addGrade(80);
+        valeria.addGrade(90);
+        valeria.addGrade(100);
+        System.out.println(valeria.getName() + "'s grade average is " + valeria.getGradeAverage());
+        System.out.println(valeria.getName() + "'s grades for the semester were " + valeria.getGrades());
+
+        Student draco = new Student("Draco");
+        draco.addGrade(60);
+        draco.addGrade(50);
+        draco.addGrade(100);
+        System.out.println(draco.getName() + "'s grade average is " + draco.getGradeAverage());
+        System.out.println(draco.getName() + "'s grades for the semester were: " + draco.getGrades());
 
     }
 }
